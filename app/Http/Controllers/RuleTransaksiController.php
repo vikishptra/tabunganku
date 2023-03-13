de48ff411726412da227befe7923dde4d639e340
shnooki <vikisahputra17@gmail.com>
2023-03-13 18:38:18 +0700

    create Or Update Transaksi

--- app/Http/Controllers/RuleTransaksiController.php
+++ app/Http/Controllers/RuleTransaksiController.php
@@ -7,10 +7,17 @@ use App\Models\RuleTransaksiBank;
 use App\Http\Requests\RuleTransaksiBankRequest;
 
 
-
 class RuleTransaksiController extends Controller{
     
     public function createRuleBank(RuleTransaksiBankRequest $request){
+        $ruleBankTransaksi = RuleTransaksiBank::where('bank_code', $request->bank_code)->first();
+        if ($request->bank_code == $ruleBankTransaksi->bank_code) {
+            $ruleBankTransaksi->bank_code = $request->bank_code;
+            $ruleBankTransaksi->rule_transaksi = $request->rule_transaksi;
+            $ruleBankTransaksi->save();
+            return $this->messagesSuccess($ruleBankTransaksi,"ok success", 200);
+
+        }
         $rule = RuleTransaksiBank::create(array_merge(
             $request->validated(),
             [
